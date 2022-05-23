f8dfec7ed846e797eb636d9d71534df32b23723c
zearkiatos <caprilespe@outlook.com>
2022-05-23 15:50:57 -0400

    All Pokemons information

--- src/client/pokemonClient.js
+++ src/client/pokemonClient.js
@@ -4,14 +4,26 @@ const getPokemons = async () => {
   try {
     const url = `${config.POKEAPI.BASE_URL}/pokemon?limit=${config.POKEAPI.LIMIT}&offset=0`;
     const response = await fetch(url);
-    const result = await response.json();
-    return result;
+    const data = await response.json();
+    return data;
   } catch (ex) {
     throw ex;
   }
 };
 
+const getPokemonByUrl = async (url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
+  catch(ex) {
+    throw ex;
+  }
+}
+
 
 export {
-    getPokemons
+    getPokemons,
+    getPokemonByUrl
 }

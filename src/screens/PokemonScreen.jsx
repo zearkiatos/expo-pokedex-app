133d3ca48b93ea0dc11ee9da94e4123183357857
zearkiatos <caprilespe@outlook.com>
2022-05-29 15:18:14 -0400

    View and navigation to the pokemon details

--- src/screens/PokemonScreen.jsx
+++ src/screens/PokemonScreen.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import React from "react";
+import { SafeAreaView, Text, StyleSheet } from "react-native";
 
-const PokemonScreen = () => {
-    return (
-        <SafeAreaView>
-            <Text>Pokemon</Text>
-        </SafeAreaView>
-    );
-}
+const PokemonScreen = ({ route, navigation }) => {
+  return (
+    <SafeAreaView>
+      <Text>Pokemon</Text>
+    </SafeAreaView>
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
 
 export default PokemonScreen;

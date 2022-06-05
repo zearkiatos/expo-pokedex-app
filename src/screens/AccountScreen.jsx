d6a97c7a9705d54c3c8e23d3ac689640c92528ae
zearkiatos <caprilespe@outlook.com>
2022-06-05 12:53:18 -0400

    My Account screen

--- src/screens/AccountScreen.jsx
+++ src/screens/AccountScreen.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import LoginForm from "../../src/components/Auth/LoginForm";
+import UserData from "../components/Auth/UserData";
 
 const AccountScreen = () => {
-    return (
-        <SafeAreaView>
-            <Text>Account</Text>
-        </SafeAreaView>
-    );
-}
+  const auth = null;
+  return <View>{auth ? <UserData /> : <LoginForm />}</View>;
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
 
 export default AccountScreen;

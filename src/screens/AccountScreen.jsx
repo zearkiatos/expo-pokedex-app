79bed2b7f10e3c3c5dcd465935e86eb71e4b7eb7
zearkiatos <caprilespe@outlook.com>
2022-06-05 16:15:30 -0400

    useAuth manage sessions with Custom Hooks and React Context

--- src/screens/AccountScreen.jsx
+++ src/screens/AccountScreen.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 import LoginForm from "../../src/components/Auth/LoginForm";
 import UserData from "../components/Auth/UserData";
+import useAuth from "../hooks/useAuth";
 
 const AccountScreen = () => {
-  const auth = null;
+  const { auth } = useAuth();
   return <View>{auth ? <UserData /> : <LoginForm />}</View>;
 };
 
